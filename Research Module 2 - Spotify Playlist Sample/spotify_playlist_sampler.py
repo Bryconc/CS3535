97c8de74214c9f24c3b486b81bc49ca1981823af
Brycon Carpenter <bryconc@users.noreply.github.com>
2015-03-17 22:12:19 -0400

    Now pulls previews from a public playlist and loads LocalAudioFiles into a list preparing for playing

--- Research Module 2 - Spotify Playlist Sample/spotify_playlist_sampler.py
+++ Research Module 2 - Spotify Playlist Sample/spotify_playlist_sampler.py
@@ -4,7 +4,16 @@ import sys
 import spotipy
 import spotipy.util as util
 
-def samplePlaylist(user, playlist_id):
+import echonest.remix.audio as audio
+
+import urllib
+
+TEST_SIZE = 5
+
+track_list = []
+samples = []
+
+def sample_playlist(user, playlist_id):
     token = util.prompt_for_user_token(user, client_id="1e9958fdd24746aea8959ccf6f724441", client_secret="a5bcdc07002c463f87a7d827c1638b91", redirect_uri="http://www.google.com")
     if token:
         sp = spotipy.Spotify(auth=token)
@@ -14,22 +23,35 @@ def samplePlaylist(user, playlist_id):
 
         tracks = playlist["tracks"]
 
-        show_tracks(tracks)
+        add_tracks(tracks)
 
-        while tracks["next"]:
-            tracks = sp.next(tracks);
-            show_tracks(tracks)
+        while tracks["next"] and len(track_list) < 5:
+            tracks = sp.next(tracks)
+            add_tracks(tracks)
 
-def show_tracks(tracks):
-    for i, item in enumerate(tracks["items"]):
-        track = item["track"]
-        print " %d %32.32s %s" % (i, track["artists"][0]["name"],track["name"])
+
+        print("Obtained %d tracks with previews" % len(track_list))
+
+def add_tracks(tracks):
+    for track in tracks["items"]:
+        if track['track']['preview_url']:
+            track_list.append(track)
+            print("Added %s" % track['track']['name'])
+
+            print("Downloading %s" % track['track']['preview_url'])
+            mp3file, headers = urllib.urlretrieve(track['track']['preview_url'], "test.mp3")
+            samples.append(audio.LocalAudioFile(mp3file))
+
+
+
+
+
 
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 2:
-        samplePlaylist(sys.argv[1], sys.argv[2])
+        sample_playlist(sys.argv[1], sys.argv[2])
     else:
         print "ERROR - Need a user and a playlist ID!"
         print "usage: python spotify_playlist_sampler.py [user] [playlistID]"
