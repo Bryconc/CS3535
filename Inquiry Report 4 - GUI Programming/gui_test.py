8ffa21d6f5ef52104f8a76d0ede8825c9ff7d22f
Brycon Carpenter <bryconc@users.noreply.github.com>
2015-04-09 12:09:30 -0400

    Updating table images

--- Inquiry Report 4 - GUI Programming/gui_test.py
+++ Inquiry Report 4 - GUI Programming/gui_test.py
@@ -41,4 +41,91 @@ def sample_checkbox():
     top.mainloop()
 
 
-sample_checkbox()
+def sample_entry():
+    title = "Sample Entry"
+
+    top = tk.Tk()
+    L = tk.Label(top, text="Sample Entry")
+    E = tk.Entry(top)
+    L.pack(side=tk.LEFT)
+    E.pack(side=tk.RIGHT)
+
+    E.insert(0, "Default Text")
+
+    top.title(title)
+    top.mainloop()
+
+
+def sample_frame():
+    title = "Sample Frame"
+
+    top = tk.Tk()
+
+    fr = tk.Frame(top)
+    fr.pack()
+
+    bFr = tk.Frame(top)
+    bFr.pack(side=tk.BOTTOM)
+
+    top1 = tk.Button(fr, text="Top 1")
+    top2 = tk.Button(fr, text="Top 2")
+    top1.pack(side=tk.LEFT)
+    top2.pack(side=tk.LEFT)
+
+    bottom = tk.Button(bFr, text="Bottom 1")
+    bottom.pack()
+
+    top.title(title)
+    top.mainloop()
+
+
+def sample_label():
+    title = "Sample Label"
+
+    top = tk.Tk()
+
+    var = tk.StringVar()
+    L = tk.Label(top, textvariable=var)
+    var.set("Sample Text in the variable")
+    L.pack()
+
+    top.title(title)
+    top.mainloop()
+
+
+def sample_listbox():
+    title = "Sample Listbox"
+
+    top = tk.Tk()
+
+    Lb = tk.Listbox(top)
+    Lb.insert(1, "Option 1")
+    Lb.insert(2, "Option 2")
+    Lb.insert(3, "Option 3")
+    Lb.pack()
+
+    top.title(title)
+    top.mainloop()
+
+
+def sample_menubutton():
+    title = "Sample Menubutton"
+
+    top = tk.Tk()
+
+    mb = tk.Menubutton(top, text="Menu")
+    mb.grid()
+
+    mb.menu = tk.Menu(mb)
+    mb["menu"] = mb.menu
+
+    mb.menu.add_checkbutton(label="Option 1")
+    mb.menu.add_checkbutton(label="Option 2")
+
+    mb.pack()
+
+    top.title(title)
+    top.mainloop()
+
+
+sample_menubutton()
